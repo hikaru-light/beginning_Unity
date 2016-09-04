3585c0520fe3a212673495dfdb23391686c88db7
Hikaru Suido <light1tus@gmail.com>
2016-09-05 03:52:39 +0900

    Add README.md and Add my files

---
+++ Assets/scripts/rotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour {
+
+    public Transform trans;
+    int a = 2;
+
+	void Update () {
+
+        rtn();
+	
+	}
+
+    void rtn(){
+
+        trans = GetComponent<Transform>();
+        trans.Rotate(new Vector3(0, 0, 1));
+
+    }
+
+    void Speedup(){
+
+
+
+    }
+}
---
+++ Assets/scripts/speed.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speed : MonoBehaviour {
+
+    void SpeedUp()
+    {
+        
+    }
+}
